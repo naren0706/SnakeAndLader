a2d7710bad814b5e7a15bdd5af29f55da1e933d9
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-13 12:17:06 +0530

    [Narenthra Kishore] Add : Two Players added

--- SnakeAndLader/Program.cs
+++ SnakeAndLader/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SnakeAndLader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SnakeAndLader player1 = new SnakeAndLader();
+            SnakeAndLader player2 = new SnakeAndLader();
+
+            string player1position = "";
+            string player2position = "";
+            
+            while (!player1.GetWon() ||  !player2.GetWon())
+            {
+                
+                player1position += player1.PlayGame() + " | ";
+                player2position += player2.PlayGame()+" | ";
+
+            }
+            Console.WriteLine("position of player1 \n"+player1position);
+            Console.WriteLine("position of player2 \n" + player2position);
+            Console.WriteLine("the total number of dice rolled " + player1.GetCount());
+            Console.WriteLine("the total number of dice rolled " + player2.GetCount());
+            //Console.WriteLine( "last postion p1 "+player1.GetPosition());
+            //Console.WriteLine( "last postion p2 "+player2.GetPosition());
+            if (player1.GetWon()) Console.WriteLine("player 1 won");
+            else if (player2.GetWon()) Console.WriteLine("player 2 won");
+
+
+        }
+    }
+}
--- SnakeAndLader/SnakeAndLader.cs
+++ SnakeAndLader/SnakeAndLader.cs
@@ -10,41 +10,76 @@ namespace SnakeAndLader
     
     internal class SnakeAndLader
     {
-        const int NO_PLAY= 0, LADDER = 1, SNAKE = 2 , WINNING_POSITION = 100,STARTING_POSITION=0;
+        const int NO_PLAY= 0, LADDER = 1, SNAKE = 2 , WINNING_POSITION = 100 , STARTING_POSITION = 0;
+        bool WON = false;
+        public void SetPosition(int position)
+        {
+            this.position = position;
+        }
+        public int GetPosition()
+        {
+            return this.position;
+        }
+        public void SetCount(int count)
+        {
+            this.count = count;
+        }
+        public int GetCount()
+        {
+            return this.count;
+        }
+        public void SetWon(bool WON)
+        {
+            this.WON = WON;
+        }
+        public bool GetWon()
+        {
+            return this.WON;
+        }
+
 
         int position = 0,count=0;
-        Random random = new Random();   
+        Random random = new Random(); 
+        
         public int DieRoll()
         {
             int diePosition = random.Next(1,7);
-            Console.WriteLine( "Die Position is"+diePosition);
-            count++;
+            //Console.WriteLine(this.position);
+            this.count++;
             return diePosition;
         }
-        public void Game()
+        public int PlayGame()
         {
-            int option = random.Next(0,3);
-            while (this.position < WINNING_POSITION)
-            {
+            //while (this.position < WINNING_POSITION)
+            //{
+                int option = random.Next(0, 3);
+
+                int dice = DieRoll();
                 switch (option)
                 {
                     case NO_PLAY:
                         break;
                     case LADDER:
-                        int dice = DieRoll();
-                        if (this.position+dice<100)
+                        
+                        if (this.position+dice<=100)
                             this.position += dice;
                         break;
                     case SNAKE:
-                        dice = DieRoll();
-                        if (this.position<STARTING_POSITION )
+                        
+                        if (this.position-dice<0 )
                             this.position = 0;
                         else
                             this.position -= dice;
                         break;
+                    default:
+                        break;
                 }
-            }
-            Console.WriteLine("the total number of dice rolled "+count);
+                if (this.position==WINNING_POSITION)
+                {
+                    WON = true;
+                }
+            return this.position;
+            //}
         } 
 
     }
