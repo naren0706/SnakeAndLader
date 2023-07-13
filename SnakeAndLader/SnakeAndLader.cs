9bc4903f4d7014aa6d0646ce6491cec031f62a2a
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-13 10:10:46 +0530

    [Narenthra Kishore] Add: New Feature Die Position

--- SnakeAndLader/SnakeAndLader.cs
+++ SnakeAndLader/SnakeAndLader.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SnakeAndLader
 {
-    int position = 0;
-
+    
     internal class SnakeAndLader
     {
+        int position = 0;
+        Random random = new Random();   
+        public void DieRoll()
+        {
+            int diePosition = random.Next(1,7);
+            Console.WriteLine( "Die POsition is"+diePosition);
+        }
+
     }
 }
