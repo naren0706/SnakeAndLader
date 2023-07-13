69106e976c307504b2b9e52bb3b15bc306c900a3
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-13 10:16:45 +0530

    [Narenthra Kishore] Add: new feature snake,lader,noposition

--- SnakeAndLader/SnakeAndLader.cs
+++ SnakeAndLader/SnakeAndLader.cs
@@ -10,12 +10,31 @@ namespace SnakeAndLader
     
     internal class SnakeAndLader
     {
+        const int NO_PLAY= 0, LADDER = 1, SNAKE = 2;
+
         int position = 0;
         Random random = new Random();   
-        public void DieRoll()
+        public int DieRoll()
         {
             int diePosition = random.Next(1,7);
             Console.WriteLine( "Die POsition is"+diePosition);
+            return diePosition;
+        }
+        public void Game()
+        {
+            int option = random.Next(0,3);
+            int dice = DieRoll();
+            switch (option)
+            {
+                case NO_PLAY:
+                    break;
+                case LADDER:
+                    this.position += dice;
+                    break;
+                case SNAKE: 
+                    this.position -= dice;
+                    break;
+            }
         }
 
     }
